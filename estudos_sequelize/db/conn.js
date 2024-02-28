d2ec17426ef977858aa66840904780575f6e1c37
Victor Pitangui <pitanguigit@gmail.com>
2024-02-28 15:51:16 -0300

    adicionar configurações iniciais

---
+++ estudos_sequelize/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Conectamos com sucesso com o Sequelize');
+
+} catch(err){
+    console.log('Não foi possivel conectar:', Error );
+}
+
+module.exports = sequelize
